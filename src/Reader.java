d591009022885fd4827863479ab1fe871496b0b4
Pyotr Spetcnaz <sgp1346@gmail.com>
2017-09-17 01:26:25 +0700

    commit for hub

--- src/Reader.java
+++ src/Reader.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Reader {
@@ -9,53 +6,73 @@ public class Reader {
     protected static ArrayList<Integer> integerValues = null;
     protected static ArrayList<String> stringValues = null;
 
-    public static void readIntFromFile(String fileName) throws IOException{
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        integerValues = new ArrayList<>(100);
+    public static boolean readIntFromFile(String fileName) throws IOException{
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            integerValues = new ArrayList<>(100);
 
-        while (reader.ready()){
-            integerValues.add(Integer.parseInt(reader.readLine()));
+            while (reader.ready()){
+                integerValues.add(Integer.parseInt(reader.readLine()));
+            }
+            reader.close();
+        } catch (FileNotFoundException e){
+            return false;
         }
-        reader.close();
+        return true;
     }
 
-    public static void readStringsFromFile(String fileName) throws IOException{
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        stringValues = new ArrayList<>(100);
+    public static boolean readStringsFromFile(String fileName) throws IOException{
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            stringValues = new ArrayList<>(100);
 
-        while (reader.ready()){
-            stringValues.add(reader.readLine());
+            while (reader.ready()){
+                stringValues.add(reader.readLine());
+            }
+            reader.close();
+        } catch (FileNotFoundException e){
+            return false;
         }
-        reader.close();
+        return true;
     }
 
-    public static void output(String fileName, int type) throws IOException{
-        FileWriter writer = new FileWriter(fileName);
-        if (type == 0) {
-            for (int i :
-                    integerValues) {
-                writer.write(i + "\r\n");
+    public static void output(String fileName, int type){
+        try {
+            FileWriter writer = new FileWriter(fileName);
+            if (type == 0) {
+                for (int i :
+                        integerValues) {
+                    writer.write(i + "\r\n");
+                }
+            } else if (type == 1){
+                for (String s :
+                        stringValues) {
+                    writer.write(s + "\r\n");
+                }
             }
-        } else if (type == 1){
-            for (String s :
-                    stringValues) {
-                writer.write(s + "\r\n");
-            }
+            writer.close();
+        } catch (IOException e){
+            System.out.println("Ошибка при выводе в файл.");
+            return;
         }
-        writer.close();
     }
 
-    public static void outputBackwards(String fileName, int type) throws IOException{
-        FileWriter writer = new FileWriter(fileName);
-        if (type == 0){
-            for (int i = integerValues.size() - 1; i >= 0; i--) {
-                writer.write(integerValues.get(i) + "\r\n");
+    public static void outputBackwards(String fileName, int type){
+        try {
+            FileWriter writer = new FileWriter(fileName);
+            if (type == 0){
+                for (int i = integerValues.size() - 1; i >= 0; i--) {
+                    writer.write(integerValues.get(i) + "\r\n");
+                }
+            } else if (type == 1){
+                for (int i = stringValues.size() - 1; i >= 0; i--) {
+                    writer.write(stringValues.get(i) + "\r\n");
+                }
             }
-        } else if (type == 1){
-            for (int i = stringValues.size() - 1; i >= 0; i--) {
-                writer.write(stringValues.get(i) + "\r\n");
-            }
+            writer.close();
+        } catch (IOException e){
+            System.out.println("Ошиюка при выводе в файл." );
+            return;
         }
-        writer.close();
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,3 @@
-import com.sun.org.apache.bcel.internal.generic.GOTO;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -8,21 +6,23 @@ import java.io.InputStreamReader;
 public class Solution {
 
     public static void main(String[] args) {
+        if (args.length < 4){
+            System.out.println("Недостаточное количество ключей.");
+            return;
+        }
         switch (args[2]){
             case "-i":
                 try {
-                    Reader.readIntFromFile(args[0]);
-                } catch (FileNotFoundException e){
-                    System.out.println("Входной файл с именем \"" + args[0] + "\" не найден, введите другое имя: ");
-                    BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-                    tryInput:
-                    try {
-                        args[0] = userInput.readLine();
-                    } catch (IOException ex){
-                        System.out.println("Ошибка ввода, введите другое имя: ");
-                        GOTO tryInput;
+                    while(!Reader.readIntFromFile(args[0]))
+                    {
+                        System.out.println("Входной файл с именем \"" + args[0] + "\" не найден, введите другое имя: ");
+                        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+                        try {
+                            args[0] = userInput.readLine();
+                        } catch (IOException ex){
+                            System.out.println("Ошибка ввода");
+                        }
                     }
-                    GOTO tryRead;
                 } catch (IOException e){
                     System.out.println("Ошибка ввода во входном файле.");
                     return;
@@ -31,38 +31,49 @@ public class Solution {
                     return;
                 }
                 Sorter.sortIntegers();
-                try {
-                    switch (args[3]){
-                        case "-a": Reader.output(args[1], 0);
+                switch (args[3]){
+                    case "-a": Reader.output(args[1], 0);
                         break;
-                        case "-d": Reader.outputBackwards(args[1], 0);
+                    case "-d": Reader.outputBackwards(args[1], 0);
                         break;
-                    }
-                } catch (IOException e){
-                    System.out.println("Ошибка при выводе в файл.");
-                    return;
+                    default:
+                        System.out.println("Неизвестные ключи.");
+                        return;
                 }
+                System.out.println("Сортировка выполнена успешно! Результат в файле \"" + args[1] + "\"");
                 break;
             case "-s":
                 try {
-                    Reader.readStringsFromFile(args[0]);
+                    while(!Reader.readStringsFromFile(args[0])){
+                        System.out.println("Входной файл с именем \"" + args[0] + "\" не найден, введите другое имя: ");
+                        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+                        try {
+                            args[0] = userInput.readLine();
+                        } catch (IOException ex){
+                            System.out.println("Ошибка ввода");
+                        }
+                    }
                 } catch (FileNotFoundException e){
                     System.out.println("Входной файл с таким именем не найден.");
                 } catch (IOException e){
                     System.out.println("Ошибка ввода во входном файле.");
                 }
                 Sorter.sortStrings();
-                try {
-                    switch (args[3]){
-                        case "-a": Reader.output(args[1], 1);
+                switch (args[3]) {
+                    case "-a":
+                        Reader.output(args[1], 1);
                         break;
-                        case "-d": Reader.outputBackwards(args[1], 1);
+                    case "-d":
+                        Reader.outputBackwards(args[1], 1);
                         break;
-                    }
-                } catch (IOException e){
-                    System.out.println("Ошибка при выводе в файл.");
+                    default:
+                        System.out.println("Неизвестные ключи.");
+                        return;
                 }
+                System.out.println("Сортировка выполнена успешно! Результат в файле \"" + args[1] + "\"");
                 break;
+            default:
+                System.out.println("Неизвестные ключи.");
         }
     }
 }
